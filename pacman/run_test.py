f2b3075a2dbc7fa622514f22fe23b828ee24c0e6
Yu Jia Tay <yujiatay@gmail.com>
2019-09-21 00:00:51 +0800

    Add pacman gym

---
+++ pacman/run_test.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import gym
+import gym_pacman
+import time
+
+env = gym.make('BerkeleyPacmanPO-v0')
+env.seed(1)
+
+done = False
+
+while True:
+    done = False
+    env.reset()
+    i = 0
+    while i < 100:
+        i += 1
+        s_, r, done, info = env.step(env.action_space.sample())
+        env.render()
+        print("Iteration over")
+        
