f87e6b6692a02fe5b6eb0d509ac90171d7575fd7
Yu Jia Tay <yujiatay@gmail.com>
2019-10-16 00:32:38 +0800

    Update save_dir default

--- train.py
+++ train.py
@@ -24,17 +24,19 @@ def parse_args():
     parser.add_argument("--num-units", type=int, default=64, help="number of units in the mlp")
     # Checkpointing
     parser.add_argument("--exp-name", type=str, default=None, help="name of the experiment")
-    parser.add_argument("--save-dir", type=str, default="C:/Users/Franklin Leong/Desktop/Backup/University/NUS "
-                                                        "Y3S1/CS3244/Project/savess/", help="directory in which "
-                                                                                            "training state and model should be saved")
-    parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="", help="directory in which training state and model are loaded")
+    parser.add_argument("--save-dir", type=str, default="./save_files/", help="directory in which training state and "
+                                                                              "model should be saved")
+    parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are "
+                                                                    "completed")
+    parser.add_argument("--load-dir", type=str, default="", help="directory in which training state and model are "
+                                                                 "loaded")
     # Evaluation
     parser.add_argument("--restore", action="store_true", default=False)
     parser.add_argument("--display", action="store_true", default=False)
     parser.add_argument("--benchmark", action="store_true", default=False)
     parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
-    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/", help="directory where benchmark data is saved")
+    parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/", help="directory where benchmark data"
+                                                                                        " is saved")
     parser.add_argument("--plots-dir", type=str, default="./learning_curves/", help="directory where plot data is saved")
     return parser.parse_args()
 
