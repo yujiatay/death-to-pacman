2ab23073d501444e4d0e67e3a966dd7e576c8b68
Franklin Leong <franklinleong303@gmail.com>
2019-10-23 19:23:08 +0800

    Testing new GIT Bash, only changed display to true

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ def parse_args():
                                                                  "loaded")
     # Evaluation
     parser.add_argument("--restore", action="store_true", default=False)
-    parser.add_argument("--display", action="store_true", default=False)
+    parser.add_argument("--display", action="store_true", default=True)
     parser.add_argument("--benchmark", action="store_true", default=False)
     parser.add_argument("--benchmark-iters", type=int, default=100000, help="number of iterations run for benchmarking")
     parser.add_argument("--benchmark-dir", type=str, default="./benchmark_files/", help="directory where benchmark data"
